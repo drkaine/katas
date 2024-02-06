98cf9f2fc1ce61d1a78adbe3a1c965f6f9f7b830
drkaine <duretkevin@live.fr>
2024-02-06 11:47:53 +0000

    Green - add rule for strike
    Add calcul of the bonus for strike

--- katas_coding/bowling_game_kata/Game.php
+++ katas_coding/bowling_game_kata/Game.php
@@ -11,7 +11,8 @@ class Game
 	public function score(): int
 	{
 		$score = 0;
-		$bonus = $this->haveBonus(0);
+		$bonus = false;
+		$strike = false;
 		$number_of_frames = count($this->frames);
 
 		for ($i = 0; $number_of_frames > $i; $i = $i + 2) {
@@ -22,7 +23,19 @@ class Game
 
 			$score += $this->frames[$i];
 
+			if (10 === $this->frames[$i]) {
+				$strike = true;
+
+				continue;
+			}
+
 			if ($i + 1 < $number_of_frames) {
+
+				if ($strike) {
+					$score += $this->frames[$i + 1] + $this->frames[$i];
+					$strike = false;
+				}
+
 				$score += $this->frames[$i + 1];
 
 				$bonus = $this->haveBonus($this->frames[$i + 1] + $this->frames[$i]);
@@ -35,6 +48,10 @@ class Game
 	public function roll(int $number_of_pins): void
 	{
 		$this->frames[] = $number_of_pins;
+
+		if (10 === $number_of_pins) {
+			$this->roll(0);
+		}
 	}
 
 	private function haveBonus(int $score): bool
--- tests/Unit/BowlingGameKataTest.php
+++ tests/Unit/BowlingGameKataTest.php
@@ -23,3 +23,10 @@ test('score with a spare and 9 pins on the third frames', function (): void {
 	$this->game->roll(9);
 	expect($this->game->score())->toBe(28);
 });
+
+test('score with a strike and 9 pins on the third  and fourth frames', function (): void {
+	$this->game->roll(10);
+	$this->game->roll(8);
+	$this->game->roll(1);
+	expect($this->game->score())->toBe(28);
+});
