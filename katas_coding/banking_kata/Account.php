64fb2766c6995faa81ceffc7198f9ae898476b31
drkaine <duretkevin@live.fr>
2024-02-06 10:17:41 +0000

    Green - Print one statement
    Print a deposit positiv

--- katas_coding/banking_kata/Account.php
+++ katas_coding/banking_kata/Account.php
@@ -8,6 +8,8 @@ class Account
 {
 	private int $balance = 0;
 
+	private array $movements;
+
 	public function deposit(int $amount): void
 	{
 		if (0 < $amount) {
@@ -15,6 +17,12 @@ class Account
 		} else {
 			$this->balance -= $amount;
 		}
+
+		$this->movements[] = [
+			'Date' => date('d.m.Y'),
+			'Amount' => '+' . $amount,
+			'Balance' => $this->balance,
+		];
 	}
 
 	public function withdraw(int $amount): void
@@ -24,6 +32,17 @@ class Account
 		} else {
 			$this->balance += $amount;
 		}
+	}
+
+	public function printStatement(): string
+	{
+		$statement = 'Date Amount Balance';
+
+		foreach ($this->movements as $movement) {
+			$statement .= '\\n' . $movement['Date'] . ' ' . $movement['Amount'] . ' ' . $movement['Balance'];
+		}
+
+		return $statement;
 	}
 
 	public function getBalance(): int
--- tests/Unit/BankingKataTest.php
+++ tests/Unit/BankingKataTest.php
@@ -27,3 +27,8 @@ test('withdraw -100', function (): void {
 	$this->account->withdraw(-100);
 	expect($this->account->getBalance())->toBe(-100);
 });
+
+test('Print statement of a deposit of 500', function (): void {
+	$this->account->deposit(500);
+	expect($this->account->printStatement())->toBe('Date Amount Balance\\n' . date('d.m.Y') . ' +500 500');
+});
