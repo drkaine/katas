38bdb44593c1963c09d5e5493aba04da5458535c
drkaine <duretkevin@live.fr>
2024-02-07 13:38:30 +0000

    Refactoring
    Separation of the calcul of the score and the setter for the score

--- katas_coding/bowling_game_kata/Game.php
+++ katas_coding/bowling_game_kata/Game.php
@@ -8,20 +8,37 @@ class Game
 {
 	private array $frames = [];
 
+	private int $score = 0;
+
 	public function score(): int
 	{
-		$score = 0;
+		$this->calculScore();
+
+		return $this->score;
+	}
+
+	public function roll(int $number_of_pins): void
+	{
+		$this->frames[] = $number_of_pins;
+
+		if (10 === $number_of_pins) {
+			$this->roll(0);
+		}
+	}
+
+	private function calculScore(): void
+	{
 		$bonus = false;
 		$strike = false;
 		$number_of_frames = count($this->frames);
 
 		for ($i = 0; $number_of_frames > $i && 20 > $i; $i = $i + 2) {
 			if ($bonus) {
-				$score += $this->frames[$i];
+				$this->score += $this->frames[$i];
 				$bonus = false;
 			}
 
-			$score += $this->frames[$i];
+			$this->score += $this->frames[$i];
 
 			if (10 === $this->frames[$i]) {
 				$strike = true;
@@ -32,25 +49,14 @@ class Game
 			if ($i + 1 < $number_of_frames) {
 
 				if ($strike) {
-					$score += $this->frames[$i + 1] + $this->frames[$i];
+					$this->score += $this->frames[$i + 1] + $this->frames[$i];
 					$strike = false;
 				}
 
-				$score += $this->frames[$i + 1];
+				$this->score += $this->frames[$i + 1];
 
 				$bonus = $this->haveBonus($this->frames[$i + 1] + $this->frames[$i]);
 			}
-		}
-
-		return $score;
-	}
-
-	public function roll(int $number_of_pins): void
-	{
-		$this->frames[] = $number_of_pins;
-
-		if (10 === $number_of_pins) {
-			$this->roll(0);
 		}
 	}
 
