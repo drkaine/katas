f37fd7a0511b7f91a1a3041edb355a5e4d430b3b
drkaine <duretkevin@live.fr>
2024-02-14 14:03:17 +0000

    Add add code source for Day6

---
+++ advent_of_craft/Day6/FizzBuzz.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Advent\Day6;
+
+use Exception;
+
+class FizzBuzz
+{
+	private int $min = 0;
+
+	private int $max = 100;
+
+	private int $fizz = 3;
+
+	private int $buzz = 5;
+
+	private int $fizzBuzz = 15;
+
+	public function fizzbuzz(int $input): string | int
+	{
+		if ($this->isOutOfRange($input)) {
+			throw new Exception('Input is out of range');
+		}
+
+		return $this->convertSafely($input);
+	}
+
+	public function convertSafely(int $input): string | int
+	{
+		if ($this->is($this->fizzBuzz, $input)) {
+			return 'FizzBuzz';
+		}
+		if ($this->is($this->fizz, $input)) {
+			return 'Fizz';
+		}
+		if ($this->is($this->buzz, $input)) {
+			return 'Buzz';
+		}
+
+		return $input;
+	}
+
+	public function is(int $divisor, int $input): bool
+	{
+		return $input % $divisor === 0;
+	}
+
+	public function isOutOfRange(int $input): bool
+	{
+		return $input <= $this->min || $input > $this->max;
+	}
+}
---
+++ tests/Unit/FizzBuzz2Test.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types = 1);
+
+use Advent\Day6\FizzBuzz;
+
+beforeEach(function (): void {
+	$this->fizzBuzz = new FizzBuzz;
+});
+
+describe('FizzBuzz should return', function (): void {
+	test('the given number for 1', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(1))->toBe(1);
+	});
+
+	test('the given number for 67', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(67))->toBe(67);
+	});
+
+	test('the given number for 82', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(82))->toBe(82);
+	});
+
+	test('Fizz for 3', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(3))->toBe('Fizz');
+	});
+
+	test('Fizz for 66', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(66))->toBe('Fizz');
+	});
+
+	test('Fizz for 99', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(99))->toBe('Fizz');
+	});
+
+	test('Buzz for 5', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(5))->toBe('Buzz');
+	});
+
+	test('Buzz for 55', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(55))->toBe('Buzz');
+	});
+
+	test('Buzz for 95', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(95))->toBe('Buzz');
+	});
+
+	test('Buzz for 15', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(15))->toBe('FizzBuzz');
+	});
+
+	test('Buzz for 30', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(30))->toBe('FizzBuzz');
+	});
+
+	test('Buzz for 45', function (): void {
+		expect($this->fizzBuzz->fizzBuzz(45))->toBe('FizzBuzz');
+	});
+
+	test('an error for 101', function (): void {
+		$fizzbuzz = new FizzBuzz;
+		expect(function () use ($fizzbuzz): void {
+			$fizzbuzz->fizzBuzz(101);
+		})->toThrow(Exception::class, 'Input is out of range');
+
+	});
+
+	test('an error for 0', function (): void {
+		$fizzbuzz = new FizzBuzz;
+		expect(function () use ($fizzbuzz): void {
+			$fizzbuzz->fizzBuzz(0);
+		})->toThrow(Exception::class, 'Input is out of range');
+
+	});
+
+	test('an error for -1', function (): void {
+		$fizzbuzz = new FizzBuzz;
+		expect(function () use ($fizzbuzz): void {
+			$fizzbuzz->fizzBuzz(-1);
+		})->toThrow(Exception::class, 'Input is out of range');
+
+	});
+});
