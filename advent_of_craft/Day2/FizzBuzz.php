112e17743164e068d24b2963e47c9b2d21e90939
drkaine <duretkevin@live.fr>
2024-02-16 13:21:30 +0000

    Solution day2

--- advent_of_craft/Day2/FizzBuzz.php
+++ advent_of_craft/Day2/FizzBuzz.php
@@ -16,6 +16,8 @@ class FizzBuzz
 
 	private int $buzz = 5;
 
+	private int $fizzBuzz = 15;
+
 	private int $modulo = 0;
 
 	private string $outOfRange = 'Input is out of range';
@@ -26,28 +28,37 @@ class FizzBuzz
 
 	public function fizzBuzz(int $input): string | int
 	{
+		$this->validateInput($input);
+
+		return $this->convertResult($input);
+	}
+
+	private function validateInput(int $input): void
+	{
 		if ($this->min >= $input || $this->max < $input) {
 			throw new Exception($this->outOfRange);
 		}
-
-		return $this->isFizz($input) . $this->isBuzz($input) ?: $input;
 	}
 
-	private function isFizz($input): string | false
+	private function convertResult(int $input): string | int
 	{
-		if ($input % $this->fizz === $this->modulo) {
-			return $this->stringFizz;
+		if ($this->is($input, $this->fizzBuzz)) {
+			return $this->stringFizz . $this->stringBuzz;
 		}
 
-		return false;
-	}
-
-	private function isBuzz($input): string | false
-	{
-		if ($input % $this->buzz === $this->modulo) {
+		if ($this->is($input, $this->buzz)) {
 			return $this->stringBuzz;
 		}
 
-		return false;
+		if ($this->is($input, $this->fizz)) {
+			return $this->stringFizz;
+		}
+
+		return $input;
+	}
+
+	private function is(int $input, int $divisor): bool
+	{
+		return $input % $divisor === $this->modulo;
 	}
 }
