400f45de8b37b6a89a69b433967427c6de415196
drkaine <duretkevin@live.fr>
2024-02-19 11:43:53 +0000

    Solution day5

--- tests/Unit/Day5/PeopleTest.php
+++ tests/Unit/Day5/PeopleTest.php
@@ -2,106 +2,26 @@
 
 declare(strict_types = 1);
 
-use Advent\Day5\Person;
-use Advent\Day5\Pet;
-use Advent\Day5\PetType;
+use Tests\Unit\Day5\PopulationTesting;
 
 describe('population', function (): void {
 
 	beforeEach(function (): void {
-		$this->population = [
-			new Person('Peter', 'Griffin', [new Pet(PetType::Cat, 'Tabby', 2)]),
-			new Person('Stewie', 'Griffin', [new Pet(PetType::Cat, 'Dolly', 3), new Pet(PetType::Dog, 'Brian', 9)]),
-			new Person('Joe', 'Swanson', [new Pet(PetType::Dog, 'Spike', 4)]),
-			new Person('Lois', 'Griffin', [new Pet(PetType::Snake, 'Serpy', 1)]),
-			new Person('Meg', 'Griffin', [new Pet(PetType::Bird, 'Tweety', 1)]),
-			new Person('Chris', 'Griffin', [new Pet(PetType::Turtle, 'Speedy', 4)]),
-			new Person('Cleveland', 'Brown', [new Pet(PetType::Hamster, 'Fuzzy', 1), new Pet(PetType::Hamster, 'Wuzzy', 2)]),
-			new Person('Glenn', 'Quagmire', []),
-		];
+		$dataTest = require 'DataTest.php';
+		$this->population = $dataTest['population'];
+		$this->formatPopulation = $dataTest['formatPopulation'];
+		$this->populationTesting = new PopulationTesting;
 	});
 
 	test('Lois owns the youngest pet', function (): void {
-		$filtered = $this->population;
+		$filteredPopulation = $this->populationTesting->getTheYoungestPetOwner($this->population);
 
-		usort($filtered, function ($person1, $person2) {
-			$person1_pet_age = $person1->pets[0]->age ?? PHP_INT_MAX;
-			$person2_pet_age = $person2->pets[0]->age ?? PHP_INT_MAX;
-
-			if ($person1_pet_age < $person2_pet_age) {
-				return -1;
-			}
-			if ($person1_pet_age > $person2_pet_age) {
-				return 1;
-			}
-
-			return 0;
-		});
-
-		$filtered = reset($filtered);
-
-		expect($filtered)->not->toBeNull();
-		expect($filtered->firstName)->toBe('Lois');
+		expect($filteredPopulation)->not->toBeNull();
+		expect($filteredPopulation->firstName)->toBe('Lois');
 	});
 
 	test('we should be able to represent people with their pets', function (): void {
-		$response = formatPopulation($this->population);
-		expect($response)->toBe('Peter Griffin who owns : Tabby \n' .
-			'Stewie Griffin who owns : Dolly Brian \n' .
-			'Joe Swanson who owns : Spike \n' .
-			'Lois Griffin who owns : Serpy \n' .
-			'Meg Griffin who owns : Tweety \n' .
-			'Chris Griffin who owns : Speedy \n' .
-			'Cleveland Brown who owns : Fuzzy Wuzzy \n' .
-			'Glenn Quagmire');
+		$response = $this->populationTesting->formatPopulation($this->population);
+		expect($response)->toBe($this->formatPopulation);
 	});
 });
-
-function formatPopulation(array $population): string
-{
-	$response = '';
-
-	for ($i = 0; count($population) > $i; $i++) {
-		$person = $population[$i];
-		$response .= $person->firstName . ' ' . $person->lastName;
-
-		if (count($person->pets) > 0) {
-			$response .= ' who owns : ';
-			foreach ($person->pets as $pet) {
-				$response .= $pet->name . ' ';
-			}
-		}
-
-		if (count($population) - 1 > $i) {
-			$response .= '\n';
-		}
-	}
-
-	return $response;
-};
-
-function comparePetAgeForPersons(Person $person1, Person $person2): int
-{
-	if (youngestPetAgeForThePerson($person1) < youngestPetAgeForThePerson($person2)) {
-		return -1;
-	}
-	if (youngestPetAgeForThePerson($person1) <= youngestPetAgeForThePerson($person2)) {
-		return 0;
-	}
-
-	return 1;
-};
-
-function youngestPetAgeForThePerson(Person $person): int
-{
-	usort($person->pets, function ($a, $b) {
-		return $a->age - $b->age;
-	});
-
-	return $person->pets[0]->age ?? PHP_INT_MAX;
-}
-
-function ageDifference(Pet $pet1, Pet $pet2): int
-{
-	return $pet1->age - $pet2->age;
-}
---
+++ tests/Unit/Day5/PopulationTesting.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Unit\Day5;
+
+use Advent\Day5\Person;
+use Advent\Day5\Pet;
+
+class PopulationTesting
+{
+	public function getTheYoungestPetOwner(array $population): Person
+	{
+		$filtered = $population;
+
+		usort($filtered, [$this, 'isTheYoungest']);
+
+		$filtered = reset($filtered);
+
+		return $filtered;
+	}
+
+	public function formatPopulation(array $population): string
+	{
+		$getResponseArray = array_map([$this, 'getPrintResume'], $population);
+
+		$getResponseString = implode('\\n', $getResponseArray);
+
+		return $getResponseString;
+	}
+
+	private function isTheYoungest($person1, $person2): int
+	{
+		$person1_pet_age = $person1->pets[0]->age ?? PHP_INT_MAX;
+		$person2_pet_age = $person2->pets[0]->age ?? PHP_INT_MAX;
+
+		if ($person1_pet_age < $person2_pet_age) {
+			return -1;
+		}
+		if ($person1_pet_age > $person2_pet_age) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	private function getPrintResume(Person $person): string
+	{
+		$response = $this->printOwnersName($person);
+
+		if (count($person->pets) > 0) {
+			$response .= ' who owns : ';
+
+			$getPetNameArray = array_map([$this, 'printPetsName'], $person->pets);
+
+			$getPetNameString = implode('', $getPetNameArray);
+
+			$response .= $getPetNameString;
+		}
+
+		return $response;
+	}
+
+	private function printOwnersName(Person $person): string
+	{
+		return $person->firstName . ' ' . $person->lastName;
+	}
+
+	private function printPetsName(Pet $pet): string
+	{
+		return $pet->name . ' ';
+	}
+}
