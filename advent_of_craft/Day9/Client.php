923883cdbac2d9ac352c3f9ad9178144f246149b
drkaine <duretkevin@live.fr>
2024-02-22 07:32:48 +0000

    Reajustement of the excercise

--- advent_of_craft/Day9/Client.php
+++ advent_of_craft/Day9/Client.php
@@ -6,31 +6,28 @@ namespace Advent\Day9;
 
 class Client
 {
-	private float $totalAmount = 0;
+	private $totalAmount;
 
-	public function __construct(
-		private readonly array $orderLines
-	) {
+	public function __construct(private array $orderLines)
+	{
 	}
 
 	public function toStatement(): string
 	{
-		$lines = array_map(function ($name, $value) {
-			return $this->formatLine($name, $value);
-		}, array_keys($this->orderLines), $this->orderLines);
-
-		return implode("\n", $lines) . "\nTotal : " . number_format($this->totalAmount, 2) . '€';
+		return implode(PHP_EOL, array_map(function ($entry) {
+			return $this->formatLine($entry['key'], $entry['value']);
+		}, $this->orderLines)) . PHP_EOL . 'Total : ' . $this->totalAmount . '€';
 	}
 
-	public function getTotalAmount(): float
+	public function getTotalAmount(): int
 	{
 		return $this->totalAmount;
 	}
 
-	private function formatLine(string $name, float $value): string
+	private function formatLine($name, $value): string
 	{
 		$this->totalAmount += $value;
 
-		return $name . ' for ' . number_format($value, 2) . '€';
+		return $name . ' for ' . $value . '€';
 	}
 }
