193eb47b8cb7aac8ed7e6de64fa1b902b54ebe7d
drkaine <duretkevin@live.fr>
2024-02-29 14:46:33 +0000

    Init day 16

---
+++ advent_of_craft/Day16/CommentAlreadyExistException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Advent\Day16;
+
+use Exception;
+
+class CommentAlreadyExistException extends Exception
+{
+}
---
+++ tests/Advent/Day16/BlogTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types = 1);
+
+uses(Tests\Advent\Day16\BlogTrait::class);
+
+use Advent\Day16\CommentAlreadyExistException;
+use Tests\Advent\Day16\ArticleBuilder;
+
+describe('Article in a blog', function (): void {
+
+	test('should add comment in an article', function (): void {
+		$article = $this->given(ArticleBuilder::anArticle());
+		$this->when(function ($article): void {
+			$article->addComment($this->commentText, $this->author);
+		}, $article);
+		$this->then(function ($article): void {
+			expect(count($article->getComments()))->
+				toBe(1);
+			$this->assertComment(
+				$article->getComments()[0],
+				$this->commentText,
+				$this->author
+			);
+		}, $article);
+	});
+
+	test('should add comment in an article containing already one', function (): void {
+		$this->generateNewComment();
+
+		$article = $this->given(ArticleBuilder::anArticle()->commented());
+		$this->when(function ($article): void {
+			$article->addComment($this->newComment, $this->newAuthor);
+		}, $article);
+		$this->then(function ($article): void {
+			expect(count($article->getComments()))->
+				toBe(2);
+			$this->assertComment(
+				$article->getComments()[0],
+				$this->commentText,
+				$this->author
+			);
+		}, $article);
+	});
+
+	test('should fail when adding existing comment', function (): void {
+		expect(function (): void {
+			$article = $this->given(ArticleBuilder::anArticle()->commented());
+			$article->addComment($this->commentText, $this->author);
+		})->toThrow(CommentAlreadyExistException::class, 'Comment already exists');
+	});
+});
