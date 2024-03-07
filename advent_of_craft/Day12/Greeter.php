151a6696711ddb020ccc3a8a3c79d0a0c08c6e02
drkaine <duretkevin@live.fr>
2024-03-07 08:29:01 +0000

    Refactoring day 12
    null object pattern

--- advent_of_craft/Day12/Greeter.php
+++ advent_of_craft/Day12/Greeter.php
@@ -6,18 +6,16 @@ namespace Advent\Day12;
 
 class Greeter
 {
-	private ?GreeterInterface $formality = null;
+	private GreeterInterface $formality;
 
-	private string $hello = 'Hello.';
+	public function __construct()
+	{
+		$this->formality = new Hello;
+	}
 
 	public function greet(): string
 	{
-		if (null === $this->formality) {
-			return $this->hello;
-		}
-
 		return $this->formality->greet();
-
 	}
 
 	public function setFormality(GreeterInterface $formality): void
